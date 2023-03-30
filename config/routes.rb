9c8fc9b3fed11ae776b401ec1435f350dc5f18f7
Megan Perry <megan.perry@strongmind.com>
2023-03-29 18:42:46 -0700

    Install devise, add user model, sign up functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
   get 'books/home'
   get 'books/index'
   get 'books/show'
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: "home#index"
   # root "articles#index"
 end
