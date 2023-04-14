d4464def14e26d8f059f3f6f4172655769b08391
Megan Perry <megan.perry@strongmind.com>
2023-04-13 18:59:42 -0700

    Add signout capability and test links for auth functionality on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :books
   get 'home/index'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'books/home'
   get 'books/index'
   get 'books/show'
